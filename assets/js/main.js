cc381bc17866c408dbf85a20260f5dd2d199e84a
mndlovu18 <mndlovu@brynmawr.edu>
2020-12-13 22:58:01 -0500

    contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -245,3 +245,24 @@ $('.owl-item .show').parent().next().children('.item').addClass('next');
 });
 
 });
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_gdrrryp';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
