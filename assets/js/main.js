cf8f94f3a692ea54e24fa0bbb8160e0f80ede93d
mndlovu18 <mndlovu@brynmawr.edu>
2021-01-17 15:34:35 -0500

    loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -267,12 +267,3 @@ document.getElementById('form')
     });
 });
 
-(function($){
-  'use strict';
-    $(window).on('load', function () {
-        if ($(".pre-loader").length > 0)
-        {
-            $(".pre-loader").fadeOut("slow");
-        }
-    });
-})(jQuery)
