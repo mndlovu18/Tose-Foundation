06e5b9e134029b9f9843c2905b4f715cc0fb1ba0
Minolta Ndlovu <62041069+mndlovu18@users.noreply.github.com>
2021-12-15 20:50:38 -0500

    edited nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,10 @@
     }
   });
 
+  $(function () {
+    $("#nav-placeholder").load("nav.html");
+  });
+
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $("#header").outerHeight() - 1;
   $(document).on(
